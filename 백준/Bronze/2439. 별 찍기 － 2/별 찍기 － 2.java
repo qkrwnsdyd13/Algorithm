6ee80d99bba0176d2a6d2e6a520645ea5e5e59a0
qkrwnsdyd13 <wnsdyddl00@naver.com>
2024-09-27 08:40:48 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 280 ms, Memory: 18600 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		
+		for ( int i = 1; i <= a; i++) {
+			for (int j = a; j > i; j-- ){
+				System.out.print(" ");
+			}
+			for (int b = 1; b <= i; b++) {
+				System.out.print("*");
+			}
+			System.out.println(" ");
+		}
+	}
+}
