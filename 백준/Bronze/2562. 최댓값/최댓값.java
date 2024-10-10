bae2e08efce1ccd7551af12f310965ba51e81ef0
qkrwnsdyd13 <wnsdyddl00@naver.com>
2024-10-11 08:01:04 +0900

    [Bronze III] Title: 최댓값, Time: 168 ms, Memory: 17636 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,23 +1,23 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        int a[] = new int[9];
-        int sum = 0;  
-
-        for ( int i = 0; i < 9; i++ ) {
-            a[i] = sc.nextInt();
-            if (a[i] > sum ) {
-                sum = a[i];
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a[] = new int[9];
+		for (int i = 0; i < a.length; i++) {
+			a[i] = sc.nextInt();
+		}
+		int max = a[0];
+		int num = 1;
+        for( int i = 1 ; i < a.length; i++ ){
+            if( a[i] > max ){
+                max = a[i];
+                num = i + 1;
             }
         }
-        System.out.println(sum);
-        for ( int i = 0; i < 9; i++) {
-            if (sum == a[i]) {
-                System.out.println(i + 1);
-            }
-        }
-    }
+        System.out.println(max);
+        System.out.println(num);
+	}
+
 }
