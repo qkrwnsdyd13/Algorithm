8387abd6111f1b2875c38a92ce78bdda975be398
qkrwnsdyd13 <wnsdyddl00@naver.com>
2024-10-11 08:00:17 +0900

    [Bronze III] Title: 최소, 최대, Time: 1604 ms, Memory: 317780 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -2,25 +2,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc =new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
 
-        int N = sc.nextInt();
-        int max, min;
-
-        int first = sc.nextInt();
-        max = first;
-        min = first;
-
-        for (int i = 1; i < N; i++) {
-            int num = sc.nextInt();
-            if (num > max) {
-                max = num;
-            }
-            if (num < min) {
-                min = num;
+        for( int i = 0; i < n; i++ ){
+            a[i] = sc.nextInt();
+        }
+        int max = a[0];
+        for( int i = 1 ; i < n; i++ ){
+            if( a[i] > max ){
+                max = a[i];
             }
         }
-
-        System.out.println(min + " " + max);
+        int min = a[0];
+        for( int i = 1; i < n; i++ ){
+            if( a[i] < min ){
+                min = a[i];
+            }
+        }
+        System.out.println(min+" "+max);
     }
 }
