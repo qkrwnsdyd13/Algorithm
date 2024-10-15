9f06192ef537aaa43cccf5bf8132c901f08538d1
qkrwnsdyd13 <wnsdyddl00@naver.com>
2024-10-16 01:32:39 +0900

    [Bronze V] Title: 문자와 문자열, Time: 168 ms, Memory: 17772 KB -BaekjoonHub

--- 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
-		int n = sc.nextInt();
-		System.out.println(s.charAt(n-1));
-	}
+        String S = sc.nextLine();
 
+        int i = sc.nextInt();
+
+        System.out.println(S.charAt(i-1));
+    }
 }
