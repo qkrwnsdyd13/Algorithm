1d1b996cde98f787c61a953aeae75b1e31cececa
qkrwnsdyd13 <wnsdyddl00@naver.com>
2024-10-16 00:02:24 +0900

    [Bronze V] Title: 문자열, Time: 184 ms, Memory: 18296 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.java
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -1,15 +1,20 @@
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new  Scanner(System.in);
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		String str;
-		for (int i = 0; i < T; i++) {
-			str = sc.next();
-			System.out.println(str.charAt(0) + "" + str.charAt(str.length()-1));
-		}
-	}
+        int b = sc.nextInt();
+        sc.nextLine();
 
+        String a[] = new String[b];
+
+        for ( int i = 0; i < b; i++ ) {
+            a[i] = sc.nextLine();
+        }
+
+        for ( int i = 0; i < b; i++ ) {
+            System.out.println(a[i].charAt(0) +""+ a[i].charAt(a[i].length() -1));
+        }
+    } 
 }
