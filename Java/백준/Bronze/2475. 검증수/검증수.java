af8f93a53e9f3c5f95e757aa1b56aa36c7e8de95
qkrwnsdyd13 <wnsdyddl00@naver.com>
2024-10-22 08:41:47 +0900

    [Bronze V] Title: 검증수, Time: 168 ms, Memory: 17688 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a[] = new int[5];
+		int sum = 0;
+		
+		for ( int i = 0; i < 5; i++) {
+			a[i] = sc.nextInt();
+			sum += Math.pow(a[i], 2);
+		}
+		System.out.println(sum%10);
+	}
+}
